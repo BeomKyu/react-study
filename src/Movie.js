0cdc29df023cdaac15c2d88a987cacb7ae76810a
bkchoi <bkchoi@MacBook-Pro.local>
2025-07-10 10:09:28 +0900

    making homepage

--- src/Movie.js
+++ src/Movie.js
@@ -18,7 +18,7 @@ function Movie({ id, year, title, summary, poster, genres }) {
                         )
                     }
                 </ul>
-                <p className="movie_summary">{summary}</p>
+                <p className="movie_summary">{summary.slice(0, 140)}...</p>
 
             </div>
         </div>
